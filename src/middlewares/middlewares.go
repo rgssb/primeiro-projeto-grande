17438cb51119a8daae94bcbba37fb9df65634721
Rodrigo <rgssbezerra@gmail.com>
2026-09-01 16:39:33 -0300

    versão 1.90

--- src/middlewares/middlewares.go
+++ src/middlewares/middlewares.go
@@ -1,1 +1,26 @@
 package middlewares
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+//Logger escreve as informacoes da requisicao no terminal
+func Logger(next http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("\n %s %s %s", r.Method, r.RequestURI, r.Host)
+		next(w, r)
+	}
+}
+
+
+
+//func (w http.ResponseWriter, r *http.Request)
+//Autenticar verifica se o usuario que esta fazendo a autenticação, esta autenticado
+func Autenticar(next http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("Autenticando...")
+		next(w, r)
+	}
+}
--- src/router/rotas/rotas.go
+++ src/router/rotas/rotas.go
@@ -1,26 +1,33 @@
 package rotas
 
 import (
+	"API/src/middlewares"
 	"net/http"
 
 	"github.com/gorilla/mux"
 )
 
-//Rota representa todas as rotas da API
+// Rota representa todas as rotas da API
 type Rota struct {
-	URI string
-	Metodo string
-	Funcao func(http.ResponseWriter, *http.Request)
+	URI                string
+	Metodo             string
+	Funcao             func(http.ResponseWriter, *http.Request)
 	RequerAutenticacao bool
 }
 
-//Configurar coloca todas as rotas dentro do router
+// Configurar coloca todas as rotas dentro do router
 func Congigurar(r *mux.Router) *mux.Router {
 	rotas := rotasUsuarios
 	rotas = append(rotas, rotalogin)
 
 	for _, rota := range rotas {
-		r.HandleFunc(rota.URI, rota.Funcao).Methods(rota.Metodo)
+		if rota.RequerAutenticacao {
+			r.HandleFunc(rota.URI, 
+				middlewares.Logger(middlewares.Autenticar(rota.Funcao)),				
+				).Methods(rota.Metodo)
+		} else {
+			r.HandleFunc(rota.URI, middlewares.Logger(rota.Funcao)).Methods(rota.Metodo)
+		}
 	}
 
 	return r

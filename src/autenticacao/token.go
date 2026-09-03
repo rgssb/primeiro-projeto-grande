52a2fe5f5a07dae054198362794e7486afa69dd1
Rodrigo <rgssbezerra@gmail.com>
2026-09-03 14:52:53 -0300

    versão 1.91

--- src/autenticacao/token.go
+++ src/autenticacao/token.go
@@ -2,6 +2,10 @@ package autenticacao
 
 import (
 	"API/src/config"
+	"errors"
+	"fmt"
+	"net/http"
+	"strings"
 	"time"
 
 	jwt "github.com/dgrijalva/jwt-go"
@@ -9,10 +13,47 @@ import (
 
 //CriarToken retorna um token assinado com as permissoes do usuario
 func CriarToken(usuarioID uint64) (string, error) {
+	
 	permissoes := jwt.MapClaims{}
 	permissoes["authorized"] = true
 	permissoes["exp"] = time.Now().Add(time.Hour *6).Unix()
 	permissoes["usuarioID"] = usuarioID
+	
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, permissoes)
 	return token.SignedString([]byte(config.SecretKey))
+}
+
+//ValidarToken valida o token recebido da requisicao
+func ValidarToken(r *http.Request) error {
+	tokenString := extrairTokens(r)
+	token, erro := jwt.Parse(tokenString, retornarChaveDeVerificacao)
+		if erro != nil {
+			return erro
+		}
+
+	if _, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+		return nil
+	}
+
+			return errors.New("Ixi Token invalido")
+}
+
+func extrairTokens(r *http.Request) string {
+	token := r.Header.Get("Authorization")
+	//Bearer andiandnqklladsa
+
+	if len(strings.Split(token, " ")) == 2 {
+		return strings.Split(token, " ")[1]
+	}
+
+	return " "
+
+}
+
+func retornarChaveDeVerificacao(token *jwt.Token) (interface{}, error) {
+	if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+		return nil, fmt.Errorf("Metodo de assinatura inesperado. %v", token.Header["alg"])
+	}
+
+	return config.SecretKey, nil
 }
--- src/middlewares/middlewares.go
+++ src/middlewares/middlewares.go
@@ -1,16 +1,17 @@
 package middlewares
 
 import (
-	"fmt"
+	"API/src/autenticacao"
+	"API/src/respostas"
 	"log"
 	"net/http"
 )
 
 //Logger escreve as informacoes da requisicao no terminal
-func Logger(next http.HandlerFunc) http.HandlerFunc {
+func Logger(proximaFuncao http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		log.Printf("\n %s %s %s", r.Method, r.RequestURI, r.Host)
-		next(w, r)
+		proximaFuncao(w, r)
 	}
 }
 
@@ -18,9 +19,12 @@ func Logger(next http.HandlerFunc) http.HandlerFunc {
 
 //func (w http.ResponseWriter, r *http.Request)
 //Autenticar verifica se o usuario que esta fazendo a autenticação, esta autenticado
-func Autenticar(next http.HandlerFunc) http.HandlerFunc {
+func Autenticar(proximaFuncao http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		fmt.Println("Autenticando...")
-		next(w, r)
+		if erro := autenticacao.ValidarToken(r); erro != nil {
+			respostas.Erro(w, http.StatusUnauthorized, erro)
+			return
+		}
+		proximaFuncao(w, r)
 	}
 }

afd2bc3ed915ce87f4a13a0f7c6c073d22ed1f8d
Rodrigo <rgssbezerra@gmail.com>
2026-09-09 14:50:40 -0300

    versão 1.95 concertada

--- src/controllers/usuarios.go
+++ src/controllers/usuarios.go
@@ -225,40 +225,41 @@ func SeguirUsuario(w http.ResponseWriter, r *http.Request) {
 
 }
 
-//PararDeSeguirUsuario faz com que um usuario pare de seguir o outro
+// PararDeSeguirUsuario faz com que um usuario pare de seguir o outro
 func PararDeSeguirUsuario(w http.ResponseWriter, r *http.Request) {
-	seguidorID, erro := autenticacao.ExtrairUsuarioID(r)
-	if erro != nil {
-		respostas.Erro(w, http.StatusUnauthorized, erro)
-		return
-	}
+    seguidorID, erro := autenticacao.ExtrairUsuarioID(r)
+    if erro != nil {
+        respostas.Erro(w, http.StatusUnauthorized, erro)
+        return
+    }
 
-	parametros := mux.Vars(r)
-	usuarioId, erro := strconv.ParseUint(parametros["usuarioId"], 10, 64)
-	if erro != nil {
-		respostas.Erro(w, http.StatusBadRequest, erro)
-		return
-	}
+    parametros := mux.Vars(r)
 
-	if seguidorID == usuarioId {
-		respostas.Erro(w, http.StatusForbidden, errors.New(
-			"Não é possivel parar de seguir você mesmo",
-		))
-		return
-	}
-  
-	db, erro := banco.Conectar()
+    usuarioID, erro := strconv.ParseUint(parametros["usuarioId"], 10, 64)
+    if erro != nil {
+        respostas.Erro(w, http.StatusBadRequest, erro)
+        return
+    }
+ 
+    if seguidorID == usuarioID {
+        respostas.Erro(w, http.StatusForbidden, errors.New(
+            "Não é possivel parar de seguir você mesmo",
+        ))
+        return
+    }
+
+    db, erro := banco.Conectar()
     if erro != nil {
         respostas.Erro(w, http.StatusInternalServerError, erro)
         return
     }
     defer db.Close()
 
-	repositorio := repositorios.NovoRepositorioDeUsuarios(db)
-	if erro = repositorio.PararDeSeguir(usuarioID, seguidorID); erro != nil {
-		respostas.Erro(w, http.StatusInternalServerError, erro)
+    repositorio := repositorios.NovoRepositorioDeUsuarios(db)
+    if erro = repositorio.PararDeSeguir(usuarioID, seguidorID); erro != nil {
+        respostas.Erro(w, http.StatusInternalServerError, erro)
         return
-	}
+    }
 
-	respostas.JSON(w, http.StatusNoContent, nil)
+    respostas.JSON(w, http.StatusNoContent, nil)
 }

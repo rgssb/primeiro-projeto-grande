90a418579555bd7df55669244c9c825023e0a9d2
Rodrigo <rgssbezerra@gmail.com>
2026-09-11 11:39:12 -0300

    versão 1.98

--- src/repositorios/usuarios.go
+++ src/repositorios/usuarios.go
@@ -223,3 +223,38 @@ func (repositorio usuarios) BuscarSeguidores(usuarioID uint64) ([]modelos.Usuari
 
 	return usuarios, nil
 }
+
+// BuscarSeguindo traz todos os seguidores que o usuario esta seguindo
+func (repositorio usuarios) BuscarSeguindo(usuarioID uint64) ([]modelos.Usuario, error) {
+	linhas, erro := repositorio.db.Query(`
+	select u.id, u.nome, u.nick, u.email, u.criadoEm 
+	from usuarios u inner join seguidores s on u.id = s.usuario_id where s.seguidor_id = ?`,
+	usuarioID,
+	)
+		if erro != nil {
+			return nil, erro
+		}
+		defer linhas.Close()
+
+		var usuarios []modelos.Usuario
+
+		for linhas.Next() {
+			var usuario modelos.Usuario
+
+			if erro = linhas.Scan(
+			&usuario.ID,
+			&usuario.Nome,
+			&usuario.Nick,
+			&usuario.Email,
+			&usuario.CriadoEm,
+		); erro != nil {
+			return nil, erro
+		}
+		
+
+		usuarios = append(usuarios, usuario)
+		}
+
+		return usuarios, nil
+
+}
